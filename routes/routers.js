c5bae9a27256720a28b1b463c62696523922fad3
black0207 <zhenying0207@163.com>
2018-01-02 10:21:52 +0800

    修改一下路由

--- routes/routers.js
+++ routes/routers.js
@@ -5,12 +5,20 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/",function(req,res) {
+router.all("/",function(req,res) {
     res.redirect("/login");
    // res.render('index', { title: 'index',name:'首页' });
 });
 
 router.get("/login",function(req,res) {
+
+
     res.render('login', { title: 'login',name:'首页' });
+
 });
+router.get("/index",function (req,res) {
+    res.render("index",{title: "首页",name:"首页"})
+});
+
+
 module.exports = router;
